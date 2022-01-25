90690abeaa1776a9abd0fb698e3551dd784c9e0b
Tsyapka1 <doc.91906@gmail.com>
2022-01-25 20:28:12 +0200

    main

---
+++ index.js
@@ -0,0 +1,20 @@
+// console.log('ab' < 'abd');  // Из-за таблицы Юникода каждая буква имеет цыфренное значение
+
+
+// let number = 5;
+
+// console.log(number > 0 && number < 20);
+
+// console.log(false || false);
+
+// let numb = 10;
+// let num = 10;
+// num = num + 1;
+
+// numb += 5;
+// console.log(num);
+
+let num1 =10;
+let num2 = 10;
+
+console.log(num1 === num2);
