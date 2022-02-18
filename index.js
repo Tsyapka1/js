6a2912ba2000ca1d62b89399b32fb2ca971f885d
Tsyapka1 <doc.91906@gmail.com>
2022-02-18 20:16:30 +0200

    Add User Description

--- index.js
+++ index.js
@@ -1,43 +1,26 @@
 'use strict';
 
-const obj = {
-  firstName: '',
-  lastName: '',
-  age: 10,
+const user = {
+  firstName: 'Dima',
+  lastName: 'Tsyapka',
+  age: 25,
+  interests: {
+    pcGame: [
+      {
+      firstGame: 'WoT',
+      firstTank: 'IS-7'
+    }
+    ],
+    myLovesSport: [{
+      fristInterests: 'Football',
+      secondInterests: 'Basketball'
+    }]
+  }
 }
 
-// console.log(obj.age);
-
-const nasaPc = {
-  ram: 938,
-  ramUnit: 'Tb',
-  hardDriveInfo: {
-    amout: 29,
-    unit: 'Pb'
-  },
-  cpus: [
-    {
-      name: 'Intel Pentium 3000',
-      clockSpeed: 4,
-      clockSpeedUnit: 'Ghz',
-      cores: [
-        {
-        id: 0,
-        clockSpeed: 4,
-
-      }
-
-      ]
-    }
-  ]
-};
 
 
-const nasaPcHDDAmout = nasaPc.hardDriveInfo.amout;
-const nasaPcHDDInfo = nasaPc.hardDriveInfo.unit;
-console.log(`${nasaPcHDDAmout} ${nasaPcHDDInfo}`);
 
-console.log(nasaPc.cpus[0].name);
+const { firstName, age, age: timeLine } = user;
 
-const { ram, ramUnit, hardDriveInfo: {amout} } = nasaPc;
-console.log(ram, ramUnit);
+console.log(firstName);
