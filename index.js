73ba664a15cdc86e129791041929d8cf3c59d85c
Tsyapka1 <doc.91906@gmail.com>
2022-02-18 18:43:56 +0200

    Add ObjDestruction

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 'use strict';
 
+const obj = {
+  firstName: '',
+  lastName: '',
+  age: 10,
+}
 
+// console.log(obj.age);
 
-function isFirsElementOfArray (arr, element) {
-  return arr[0] === element;
-};
+const nasaPc = {
+  ram: 938,
+  ramUnit: 'Tb',
+  hardDriveInfo: {
+    amout: 29,
+    unit: 'Pb'
+  },
+  cpus: [
+    {
+      name: 'Intel Pentium 3000',
+      clockSpeed: 4,
+      clockSpeedUnit: 'Ghz',
+      cores: [
+        {
+        id: 0,
+        clockSpeed: 4,
 
-const arr = [0,2,3,4];
-const arr2 = Array(500000).fill(null).map((_,index) => index + 1)
-const number1 = 8;
-const number2 = 0;
+      }
 
-
-console.time('0(1) big array');
-const result1 = isFirsElementOfArray(arr, number1);
-console.timeEnd('0(1)');
-
-console.time('0(1)')
-const result2 = isFirsElementOfArray(arr, number2);
-console.timeEnd('0(1) big array');
-
-//0 (n)
-
-function isElementInArray (arr, elem) {
-  for(let i = 0; i<arr.lenght; i++) {
-    if(elem === arr[i]) {
-      return true;
+      ]
     }
-  }
-  return false;
+  ]
 };
 
-console.time('O(n) best case')
-const oNworst = isElementInArray(arr2, 49999);
-console.timeEnd('O(n) best case')
+
+const nasaPcHDDAmout = nasaPc.hardDriveInfo.amout;
+const nasaPcHDDInfo = nasaPc.hardDriveInfo.unit;
+console.log(`${nasaPcHDDAmout} ${nasaPcHDDInfo}`);
+
+console.log(nasaPc.cpus[0].name);
+
+const { ram, ramUnit, hardDriveInfo: {amout} } = nasaPc;
+console.log(ram, ramUnit);
