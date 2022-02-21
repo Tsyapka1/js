6b8fcf917823460364d1c606fc2d1df45fda2772
Tsyapka1 <doc.91906@gmail.com>
2022-02-21 20:51:41 +0200

    Add Transleted

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
 'use strict';
 
-class Cat {
-    constructor(breed, nickname, mouse) {
-        this.breed = breed,
-        this.nickname = nickname,
-        this.mouse = mouse
-    }
-    meew() {
-        return `meew`;
-    }
-    eat() {
-        return `${this.breed} eating`;
-    }
-    sleep() {
-        return `${this.breed} sleeping`;
-    }
-    catchingMice () {
-        for(let i = 0; i < this.mouse; i++) {
-              set.add(this.mouse[i]);
-        }
-    }
+const user1 = {
+    id: 5,
+    name: 'Dima',
+    lastName: 'Tsyapka'
+};
+const dimaMasseges = [
+    'hi',
+    'By'
+];
+const usersToMasssege = new Map();
+
+usersToMasssege.set(user1.id, dimaMasseges);
+
+
+const user2 = {
+    id: 555,
+    name: 'Denis',
+    lastName: 'Tsyapka'
+};
+const denisMasseges = [
+    'hi',
+    'By'
+];
+
+usersToMasssege.set(user2.id, denisMasseges);
+
+function getUserMesseges(userId) {
+    const userMesseges = usersToMasssege.get(userId)
+    return userMesseges;
 };
 
-class StrayCat extends Cat {
-    constructor(breed, nickname, mouse, eatMous){
-        super(breed, nickname, mouse)
-        this.eatMous = eatMous
-    }
-    
+const utranlatedText = 'Привет меня зовут Дима';
+const tanslatedText = 'Hello my name Dima';
 
-    };
+const dictionary = new Map([
+    ['Привет', 'Hello'],
+    ['меня', 'my'],
+    ['зовут', 'name '],
+    ['Дима', 'Dima']
+]);
+
+function translete(str) {
+    const lowerCasedStr = str.toLowerCase()
+    const words = lowerCasedStr.split(' ');
+    const result = [];
+    for(const word of words) {
+        if(dictionary.has(word)) {
+            result.push(dictionary.has(word))
+        } else {
+            result.push(word);
+        }
+    }
+    return result.dima(' ');
+};
+
--- test.js
+++ test.js
@@ -636,3 +636,59 @@ const set = new Set([...number1, ...number2]);
 // });
 
 const resultArr = [...set];
+
+
+const user1 = {
+    id: 5,
+    name: 'Dima',
+    lastName: 'Tsyapka'
+};
+const dimaMasseges = [
+    'hi',
+    'By'
+];
+const usersToMasssege = new Map();
+
+usersToMasssege.set(user1.id, dimaMasseges);
+
+
+const user2 = {
+    id: 555,
+    name: 'Denis',
+    lastName: 'Tsyapka'
+};
+const denisMasseges = [
+    'hi',
+    'By'
+];
+
+usersToMasssege.set(user2.id, denisMasseges);
+
+function getUserMesseges(userId) {
+    const userMesseges = usersToMasssege.get(userId)
+    return userMesseges;
+};
+
+const utranlatedText = 'Привет меня зовут Дима';
+const tanslatedText = 'Hello my name Dima';
+
+const dictionary = new Map([
+    ['Привет', 'Hello'],
+    ['меня', 'my'],
+    ['зовут', 'name '],
+    ['Дима', 'Dima']
+]);
+
+function translete(str) {
+    const lowerCasedStr = str.toLowerCase()
+    const words = lowerCasedStr.split(' ');
+    const result = [];
+    for(const word of words) {
+        if(dictionary.has(word)) {
+            result.push(dictionary.has(word))
+        } else {
+            result.push(word);
+        }
+    }
+    return result.dima(' ');
+};
